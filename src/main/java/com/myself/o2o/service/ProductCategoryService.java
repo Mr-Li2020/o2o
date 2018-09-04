c4a77f236de853bbd94c31b9776bcd120a2179fe
liningbo <1450595113@qq.com>
2018-09-04 22:46:57 +0800

    商品类别列表之service层通过shopId查询店铺商品类别的方法的实现

---
+++ src/main/java/com/myself/o2o/service/ProductCategoryService.java
@@ -0,0 +1,15 @@
+package com.myself.o2o.service;
+
+import com.myself.o2o.entity.ProductCategory;
+
+import java.util.List;
+
+public interface ProductCategoryService {
+    /**
+     * 查询指定某个店铺下的所有商品类别信息
+     *
+     * @param shopId
+     * @return
+     */
+    List<ProductCategory> getProductCategoryList(long shopId);
+}
