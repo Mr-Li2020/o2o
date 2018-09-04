011cc369b03b9cd0867484170728462805aecea7
liningbo <1450595113@qq.com>
2018-09-04 22:47:54 +0800

    商品类别列表之service层通过shopId查询店铺商品类别的方法的实现

--- src/main/java/com/myself/o2o/service/impl/ProductCategoryServiceImpl.java
+++ src/main/java/com/myself/o2o/service/impl/ProductCategoryServiceImpl.java
@@ -7,8 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 @Service
-public class ProducctCategoryServiceImpl implements ProductCategoryService {
+public class ProductCategoryServiceImpl implements ProductCategoryService {
     @Autowired
     private ProductCategoryDao productCategoryDao;
 
