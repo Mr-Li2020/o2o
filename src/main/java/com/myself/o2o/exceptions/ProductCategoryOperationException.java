fbf0122572ee669a48ea095430fa6c3a61740c01
liningbo <1450595113@qq.com>
2018-09-05 11:48:12 +0800

    商品类别的异常信息类

---
+++ src/main/java/com/myself/o2o/exceptions/ProductCategoryOperationException.java
@@ -0,0 +1,7 @@
+package com.myself.o2o.exceptions;
+
+public class ProductCategoryOperationException extends RuntimeException {
+    public ProductCategoryOperationException(String msg){
+        super(msg);
+    }
+}
