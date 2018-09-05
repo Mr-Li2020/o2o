53ec237115cc738f9da8b9ad8a8a23edbca84405
liningbo <1450595113@qq.com>
2018-09-05 13:29:07 +0800

    商品类别controller层之批量添加商品类别和删除指定商品类别方法实现

--- src/main/java/com/myself/o2o/controller/shopadmin/ProductCategoryManagementController.java
+++ src/main/java/com/myself/o2o/controller/shopadmin/ProductCategoryManagementController.java
@@ -1,18 +1,23 @@
 package com.myself.o2o.controller.shopadmin;
 
+import com.myself.o2o.dto.ProductCategoryExecution;
 import com.myself.o2o.dto.Result;
 import com.myself.o2o.entity.ProductCategory;
 import com.myself.o2o.entity.Shop;
 import com.myself.o2o.enums.ProductCategoryStateEnum;
+import com.myself.o2o.exceptions.ProductCategoryOperationException;
 import com.myself.o2o.service.ProductCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/shopadmin")
@@ -20,13 +25,9 @@ public class ProductCategoryManagementController {
     @Autowired
     private ProductCategoryService productCategoryService;
 
-    @RequestMapping(value = "getProductCategoryList", method = RequestMethod.GET)
+    @RequestMapping(value = "getproductcategorylist", method = RequestMethod.GET)
     @ResponseBody
     private Result<List<ProductCategory>> getProductCategoryList(HttpServletRequest request) {
-        //To be removed
-        Shop shop = new Shop();
-        shop.setShopId(1L);
-        request.getSession().setAttribute("currentShop", shop);
 
         Shop currentShop = (Shop) request.getSession().getAttribute("currentShop");
         List<ProductCategory> productCategoryList = null;
@@ -38,4 +39,62 @@ public class ProductCategoryManagementController {
             return new Result<List<ProductCategory>>(false, ps.getState(), ps.getStateInfo());
         }
     }
+
+    @RequestMapping(value = "/addproductcategorys", method = RequestMethod.POST)
+    @ResponseBody
+    private Map<String, Object> addProductCategorys(@RequestBody List<ProductCategory> productCategoryList, HttpServletRequest request) {
+        Map<String, Object> modelMap = new HashMap<String, Object>();
+        Shop currentShop = (Shop) request.getSession().getAttribute("currentShop");
+        for (ProductCategory pc : productCategoryList) {
+            pc.setShopId(currentShop.getShopId());
+        }
+        if (productCategoryList != null && productCategoryList.size() > 0) {
+            try {
+                ProductCategoryExecution productCategoryExecution = productCategoryService.batchAddProductCategory(productCategoryList);
+                if (productCategoryExecution.getState() == ProductCategoryStateEnum.SUCCESS.getState()) {
+                    modelMap.put("success", true);
+                } else {
+                    modelMap.put("success", false);
+                    modelMap.put("errMsg", productCategoryExecution.getStateInfo());
+                }
+            } catch (RuntimeException e) {
+                modelMap.put("success", false);
+                modelMap.put("errMsg", e.toString());
+                return modelMap;
+            }
+        } else {
+            modelMap.put("success", false);
+            modelMap.put("errMsg", "请至少输入一个商品类别");
+        }
+
+        return modelMap;
+    }
+
+    @RequestMapping(value = "/removeproductcategory", method = RequestMethod.POST)
+    @ResponseBody
+    private Map<String, Object> removeProductCategory(Long productCategoryId, HttpServletRequest request) {
+        Map<String, Object> modelMap = new HashMap<String, Object>();
+        if (productCategoryId != null && productCategoryId > 0) {
+            try {
+                Shop currentShop = (Shop) request.getSession().getAttribute("currentShop");
+                ProductCategoryExecution productCategoryExecution = productCategoryService.deleteProductCategory(productCategoryId, currentShop.getShopId());
+                if (productCategoryExecution.getState() == ProductCategoryStateEnum.SUCCESS.getState()) {
+                    modelMap.put("success", true);
+                } else {
+                    modelMap.put("success", false);
+                    modelMap.put("errMsg", productCategoryExecution.getStateInfo());
+                }
+            } catch (ProductCategoryOperationException e) {
+                modelMap.put("success", false);
+                modelMap.put("errMsg", e.toString());
+                return modelMap;
+            }
+        } else {
+            modelMap.put("success", false);
+            modelMap.put("errMsg", "请至少选择一个商品类别");
+        }
+
+        return modelMap;
+    }
+
 }
