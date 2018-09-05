2a881dd606d5d02e9a48d665dde7e1ad5e39451b
liningbo <1450595113@qq.com>
2018-09-05 13:28:04 +0800

    商品类别controller之路由添加

--- src/main/java/com/myself/o2o/controller/shopadmin/ShopAdminController.java
+++ src/main/java/com/myself/o2o/controller/shopadmin/ShopAdminController.java
@@ -22,4 +22,8 @@ public class ShopAdminController {
         return "shop/shopmanagement";
     }
 
+    @RequestMapping(value = "/productcategorymanagement")
+    public String productCategoryManage(){
+        return "shop/productcategorymanagement";
+    }
 }
