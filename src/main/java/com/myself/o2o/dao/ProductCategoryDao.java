3a8b7f43b1c574e816444839d88b082810a16ff7
liningbo <1450595113@qq.com>
2018-09-05 11:50:19 +0800

    商品类别批量增加的dao层实现

--- src/main/java/com/myself/o2o/dao/ProductCategoryDao.java
+++ src/main/java/com/myself/o2o/dao/ProductCategoryDao.java
@@ -11,4 +11,11 @@ public interface ProductCategoryDao {
      * @return
      */
     List<ProductCategory> queryProductCategoryList(long shopId);
+
+    /**
+     * 批量新增商品类别
+     * @param productCategoryList
+     * @return
+     */
+    int batchInsertProductCategory(List<ProductCategory> productCategoryList);
 }
