bbf7085164ebef784ba1c48f3cdd9b9e95a7b699
liningbo <1450595113@qq.com>
2018-09-06 16:34:09 +0800

    商品dao层之添加商品

---
+++ src/main/java/com/myself/o2o/dao/ProductDao.java
@@ -0,0 +1,12 @@
+package com.myself.o2o.dao;
+
+import com.myself.o2o.entity.Product;
+
+public interface ProductDao {
+    /**
+     * 插入商品
+     * @param product
+     * @return
+     */
+    int insertProduct(Product product);
+}
