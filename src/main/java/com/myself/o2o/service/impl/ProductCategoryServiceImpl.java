a808ffcbe4b05f68a3a34aef312cb9830a8b1856
liningbo <1450595113@qq.com>
2018-09-08 19:11:55 +0800

    商品service层实现之解除商品和某商品类别之间的联系

--- src/main/java/com/myself/o2o/service/impl/ProductCategoryServiceImpl.java
+++ src/main/java/com/myself/o2o/service/impl/ProductCategoryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.myself.o2o.service.impl;
 
 import com.myself.o2o.dao.ProductCategoryDao;
+import com.myself.o2o.dao.ProductDao;
 import com.myself.o2o.dto.ProductCategoryExecution;
 import com.myself.o2o.entity.ProductCategory;
 import com.myself.o2o.enums.ProductCategoryStateEnum;
@@ -16,6 +17,9 @@ import java.util.List;
 public class ProductCategoryServiceImpl implements ProductCategoryService {
     @Autowired
     private ProductCategoryDao productCategoryDao;
+
+    @Autowired
+    private ProductDao productDao;
 
     @Override
     public List<ProductCategory> getProductCategoryList(long shopId) {
@@ -44,7 +48,16 @@ public class ProductCategoryServiceImpl implements ProductCategoryService {
     @Override
     @Transactional
     public ProductCategoryExecution deleteProductCategory(long productCategoryId, long shopId) throws ProductCategoryOperationException {
-        //TODO 将此商品类别下的商品的类别id置为空
+        //解除tb_product里面的商品与该productcategoryId的关联
+        try{
+            int effectedNum = productDao.updateProductCategoryToNull(productCategoryId);
+            if(effectedNum<0){
+                throw new ProductCategoryOperationException("商品类别更新失败");
+            }
+        }catch (Exception e){
+            throw new ProductCategoryOperationException("deleteProductCategory error:"+e.getMessage());
+        }
+        //删除该productCategory
         try{
             int effectedNum = productCategoryDao.deleteProductCategory(productCategoryId, shopId);
             if(effectedNum<=0){
