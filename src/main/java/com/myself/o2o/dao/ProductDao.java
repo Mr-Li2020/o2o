8ca017960ae54cc89ba34115670a4441e4973a4b
liningbo <1450595113@qq.com>
2018-09-08 12:40:31 +0800

    商品dao层之通过id查询商品和更新商品信息

--- src/main/java/com/myself/o2o/dao/ProductDao.java
+++ src/main/java/com/myself/o2o/dao/ProductDao.java
@@ -1,12 +1,36 @@
 package com.myself.o2o.dao;
 
+import com.myself.o2o.dto.ImageHolder;
+import com.myself.o2o.dto.ProductExecution;
 import com.myself.o2o.entity.Product;
+import com.myself.o2o.exceptions.ProductOperationException;
+
+import java.util.List;
 
 public interface ProductDao {
     /**
      * 插入商品
+     *
      * @param product
      * @return
      */
     int insertProduct(Product product);
+
+    /**
+     * 通过商品Id查询唯一的商品信息
+     *
+     * @param productId
+     * @return
+     */
+    Product queryProductById(long productId);
+
+    /**
+     * 更新商品信息
+     *
+     * @param product
+     * @return
+     */
+    int updateProduct(Product product);
+
+
 }
