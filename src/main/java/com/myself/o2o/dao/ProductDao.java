f11bec6a1a4cbf5607cad503aea4c5821477122e
liningbo <1450595113@qq.com>
2018-09-08 19:14:26 +0800

    商品dao层之删除商品列表前将该商品的商品类别Id置为空

--- src/main/java/com/myself/o2o/dao/ProductDao.java
+++ src/main/java/com/myself/o2o/dao/ProductDao.java
@@ -4,6 +4,7 @@ import com.myself.o2o.dto.ImageHolder;
 import com.myself.o2o.dto.ProductExecution;
 import com.myself.o2o.entity.Product;
 import com.myself.o2o.exceptions.ProductOperationException;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -32,5 +33,30 @@ public interface ProductDao {
      */
     int updateProduct(Product product);
 
+    /**
+     * 查询商品列表并分页,可输入的条件有:商品名(模糊) 商品状态  店铺Id 店铺类别
+     *
+     * @param productCondition
+     * @param rowIndex
+     * @param pageSize
+     * @return
+     */
+    List<Product> queryProductList(@Param("productCondition") Product productCondition, @Param("rowIndex") int rowIndex, @Param("pageSize") int pageSize);
+
+    /**
+     * 查询对应的商品总数
+     *
+     * @param productCondition
+     * @return
+     */
+    int queryProductCount(@Param("productCondition") Product productCondition);
+
+    /**
+     * 删除商品类别之前,将商品类别ID置为空
+     *
+     * @param productCategoryId
+     * @return
+     */
+    int updateProductCategoryToNull(long productCategoryId);
 
 }
