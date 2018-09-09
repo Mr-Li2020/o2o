842e544db6361fc73217788fa1a4b78a1e8d84f6
liningbo <1450595113@qq.com>
2018-09-09 23:44:45 +0800

    头条单元测试

---
+++ src/test/java/com/myself/o2o/dao/HeadLineDaoTest.java
@@ -0,0 +1,19 @@
+package com.myself.o2o.dao;
+
+import com.myself.o2o.BaseTest;
+import com.myself.o2o.entity.HeadLine;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class HeadLineDaoTest extends BaseTest {
+    @Autowired
+    private HeadLineDao headLineDao;
+
+    @Test
+    public void testQueryHeadLine(){
+        List<HeadLine> headLineList = headLineDao.queryHeadLine(new HeadLine());
+        System.out.println(headLineList.size());
+    }
+}
