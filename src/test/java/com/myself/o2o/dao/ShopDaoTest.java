05843a65861ad71ddcb4f4ab7def472b36476382
liningbo <1450595113@qq.com>
2018-09-11 23:35:40 +0800

    店铺dao层单元测试之测试一级店铺下面的二级店铺列表

--- src/test/java/com/myself/o2o/dao/ShopDaoTest.java
+++ src/test/java/com/myself/o2o/dao/ShopDaoTest.java
@@ -16,8 +16,9 @@ import static org.junit.Assert.assertEquals;
 public class ShopDaoTest extends BaseTest {
     @Autowired
     private ShopDao shopDao;
+
     @Test
-    public void testInsertShop(){
+    public void testInsertShop() {
         Shop shop = new Shop();
         PersonInfo owner = new PersonInfo();
         Area area = new Area();
@@ -37,41 +38,40 @@ public class ShopDaoTest extends BaseTest {
         shop.setEnableStatus(1);
         shop.setAdvice("审核中");
         int effectedNum = shopDao.insertShop(shop);
-        assertEquals(1,effectedNum);
+        assertEquals(1, effectedNum);
     }
+
     @Test
-    public void testUpdateShop(){
+    public void testUpdateShop() {
         Shop shop = new Shop();
         shop.setShopId(1L);
         shop.setShopDesc("测试描述");
         shop.setShopAddr("测试地址");
         shop.setLastEditTime(new Date());
         int effectedNum = shopDao.updateShop(shop);
-        assertEquals(1,effectedNum);
+        assertEquals(1, effectedNum);
     }
+
     @Test
-    public void testQueryByShopId(){
+    public void testQueryByShopId() {
         long shopId = 1;
         Shop shop = shopDao.queryByShopId(shopId);
-        System.out.println("areaId:"+shop.getArea().getAreaId());
-        System.out.println("areaName:"+shop.getArea().getAreaName());
+        System.out.println("areaId:" + shop.getArea().getAreaId());
+        System.out.println("areaName:" + shop.getArea().getAreaName());
     }
+
     @Test
-    public void testQueryShopListAndCount(){
+    public void testQueryShopListAndCount() {
         Shop shopCondition = new Shop();
-        PersonInfo owner = new PersonInfo();
-        owner.setUserId(1L);
-        shopCondition.setOwner(owner);
-        List<Shop> shopList = shopDao.queryShopList(shopCondition, 0, 3);
+        ShopCategory childCategory = new ShopCategory();
+        ShopCategory parentCategory = new ShopCategory();
+        parentCategory.setShopCategoryId(1L);
+        childCategory.setParent(parentCategory);
+        shopCondition.setShopCategory(childCategory);
+        List<Shop> shopList = shopDao.queryShopList(shopCondition, 0, 6);
         int count = shopDao.queryShopCount(shopCondition);
-        System.out.println("店铺列表大小:"+shopList.size());
-        System.out.println("店铺总数:"+count);
-        ShopCategory sc = new ShopCategory();
-        sc.setShopCategoryId(2L);
-        shopCondition.setShopCategory(sc);
-        shopList = shopDao.queryShopList(shopCondition, 0, 1);
-        count = shopDao.queryShopCount(shopCondition);
-        System.out.println("店铺列表大小:"+shopList.size());
-        System.out.println("店铺总数:"+count);
+        System.out.println("店铺列表大小:" + shopList.size());
+        System.out.println("店铺总数:" + count);
+
     }
 }
